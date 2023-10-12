37b69aa9b7b5e860249c94a8591c80af2496a79b
Kasavaraju-v <114193191+Kasavaraju-v@users.noreply.github.com>
2023-10-12 16:38:21 -0400

    Develop (#2)
    * added initial code for assignment 4
    
    * updated code for assignment 4
    
    * updated configurations

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/c-robinson/iplib"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+	"net"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,6 +19,27 @@ func main() {
 		cidrBlock := conf.Require("cidrBlock")
 		vpcName := conf.Require("vpcName")
 		destinationBlock := conf.Require("destinationBlock")
+
+		internetGatewayName := conf.Require("internetGatewayName")
+		publicSubnetName := conf.Require("publicSubnetName")
+		privateSubnetName := conf.Require("privateSubnetName")
+		publicRouteTableName := conf.Require("publicRouteTableName")
+		privateRouteTableName := conf.Require("privateRouteTableName")
+		publicRTAName := conf.Require("publicRTAName")
+		privateRTAName := conf.Require("privateRTAName")
+
+		parts := strings.Split(cidrBlock, "/")
+		ip := parts[0]
+		maskStr := parts[1]
+		mask, _ := strconv.Atoi(maskStr)
+
+		n := iplib.NewNet4(net.ParseIP(ip), mask)
+		subnets, _ := n.Subnet(24)
+
+		subnetStrings := make([]string, len(subnets))
+		for i, subnet := range subnets {
+			subnetStrings[i] = subnet.String()
+		}
 
 		available, err := aws.GetAvailabilityZones(ctx, &aws.GetAvailabilityZonesArgs{
 			State: pulumi.StringRef("available"),
@@ -39,13 +63,13 @@ func main() {
 		// Create Public Subnets
 		publicSubnets := make([]*ec2.Subnet, 0, subnetCount)
 		for i := 0; i < subnetCount; i++ {
-			publicSubnet, err := ec2.NewSubnet(ctx, "public-subnet- "+strconv.Itoa(i+1), &ec2.SubnetArgs{
+			publicSubnet, err := ec2.NewSubnet(ctx, publicSubnetName+"-"+strconv.Itoa(i+1), &ec2.SubnetArgs{
 				VpcId:               vpc.ID(),
-				CidrBlock:           pulumi.String("10.0." + strconv.Itoa(i+1) + ".0/24"),
+				CidrBlock:           pulumi.String(subnetStrings[i]),
 				AvailabilityZone:    pulumi.String(available.Names[i]),
 				MapPublicIpOnLaunch: pulumi.Bool(true),
 				Tags: pulumi.StringMap{
-					"Name": pulumi.String("public-subnet- " + strconv.Itoa(i+1)),
+					"Name": pulumi.String(publicSubnetName + "-" + strconv.Itoa(i+1)),
 				},
 			})
 			if err != nil {
@@ -57,12 +81,12 @@ func main() {
 		// Create Private Subnets
 		privateSubnets := make([]*ec2.Subnet, 0, subnetCount)
 		for i := 0; i < subnetCount; i++ {
-			privateSubnet, err := ec2.NewSubnet(ctx, "private-subnet- "+strconv.Itoa(i+1), &ec2.SubnetArgs{
+			privateSubnet, err := ec2.NewSubnet(ctx, privateSubnetName+"-"+strconv.Itoa(i+1), &ec2.SubnetArgs{
 				VpcId:            vpc.ID(),
-				CidrBlock:        pulumi.String("10.0." + strconv.Itoa(i+4) + ".0/24"),
+				CidrBlock:        pulumi.String(subnetStrings[i+subnetCount]),
 				AvailabilityZone: pulumi.String(available.Names[i]),
 				Tags: pulumi.StringMap{
-					"Name": pulumi.String("private-subnet- " + strconv.Itoa(i+1)),
+					"Name": pulumi.String(privateSubnetName + "-" + strconv.Itoa(i+1)),
 				},
 			})
 			if err != nil {
@@ -72,10 +96,10 @@ func main() {
 		}
 
 		// Create a Internet gateway
-		internetGateway, err := ec2.NewInternetGateway(ctx, "Internet-Gateway", &ec2.InternetGatewayArgs{
+		internetGateway, err := ec2.NewInternetGateway(ctx, internetGatewayName, &ec2.InternetGatewayArgs{
 			VpcId: vpc.ID(),
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("Internet-Gateway"),
+				"Name": pulumi.String(internetGatewayName),
 			},
 		})
 		if err != nil {
@@ -83,10 +107,10 @@ func main() {
 		}
 
 		//Create a Public Route Table
-		publicRouteTable, err := ec2.NewRouteTable(ctx, "public-route-table", &ec2.RouteTableArgs{
+		publicRouteTable, err := ec2.NewRouteTable(ctx, publicRouteTableName, &ec2.RouteTableArgs{
 			VpcId: vpc.ID(),
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("public-route-table"),
+				"Name": pulumi.String(publicRouteTableName),
 			},
 		})
 		if err != nil {
@@ -104,10 +128,10 @@ func main() {
 		}
 
 		// Create a Private Route Table
-		privateRouteTable, err := ec2.NewRouteTable(ctx, "private-route-table", &ec2.RouteTableArgs{
+		privateRouteTable, err := ec2.NewRouteTable(ctx, privateRouteTableName, &ec2.RouteTableArgs{
 			VpcId: vpc.ID(),
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("private-route-table"),
+				"Name": pulumi.String(privateRouteTableName),
 			},
 		})
 		if err != nil {
@@ -115,7 +139,7 @@ func main() {
 		}
 		// Associate the Public Subnets to the Public Route Table.
 		for i, subnet := range publicSubnets {
-			_, err := ec2.NewRouteTableAssociation(ctx, "publicRouteAssociation-"+strconv.Itoa(i+1), &ec2.RouteTableAssociationArgs{
+			_, err := ec2.NewRouteTableAssociation(ctx, publicRTAName+"-"+strconv.Itoa(i+1), &ec2.RouteTableAssociationArgs{
 				SubnetId:     subnet.ID(),
 				RouteTableId: publicRouteTable.ID(),
 			})
@@ -126,7 +150,7 @@ func main() {
 
 		// Associate the Private Subnets to the Private Route Table.
 		for i, subnet := range privateSubnets {
-			_, err := ec2.NewRouteTableAssociation(ctx, "privateRoutTableAssociation-"+strconv.Itoa(i+1), &ec2.RouteTableAssociationArgs{
+			_, err := ec2.NewRouteTableAssociation(ctx, privateRTAName+"-"+strconv.Itoa(i+1), &ec2.RouteTableAssociationArgs{
 				SubnetId:     subnet.ID(),
 				RouteTableId: privateRouteTable.ID(),
 			})
